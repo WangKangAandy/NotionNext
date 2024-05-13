0b411130ca703c6ee41ab31ae1b43936cc4dcf30
WangKangAandy <158999716+WangKangAandy@users.noreply.github.com>
2024-05-13 09:21:28 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,10 +4,10 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '在路上', url: '' },
+  //HEO_NOTICE_BAR: [
+   // { title: '在路上', url: '' },
     //{ title: '访问文档中心获取更多帮助', url: '' }
-  ],
+  //],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: true,
@@ -15,21 +15,21 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  //HEO_HERO_TITLE_1: '分享编程',
+  //HEO_HERO_TITLE_2: '与思维认知',
+  //HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  //HEO_HERO_TITLE_4: '新版上线',
+  //HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  //HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  //HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  //HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  //HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  //HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  //HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
@@ -127,7 +127,7 @@ const CONFIG = {
   HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
+  HEO_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
   HEO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   HEO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
